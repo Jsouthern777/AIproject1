5da22a0d4c5e15572be7c1818b328dc7a65bd8cf
Jsouthern777 <southernjj21@gcc.edu>
2024-03-10 16:23:24 -0400

    Initial commit

---
+++ src/SchedulingProblem.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * What we need:
+ * Varaiables: Hours
+ * Values: People
+ * Constraints: No more than 8 consecutive hours, 12 hours between consecutive shifts, random availability
+ * Number of employees required will be randomized
+ * Number of shifts per day will be randomized
+ */
+public class SchedulingProblem {
+    //List of employees that will be of random length
+    private ArrayList<List> employees;
+    //List of required shifts that will be of random length
+    private ArrayList<List> shifts;
+
+}
