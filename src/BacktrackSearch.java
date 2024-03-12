f809ba3971b5c1e1d88b1fafccb7d307b829135d
Jsouthern777 <southernjj21@gcc.edu>
2024-03-11 21:28:40 -0400

    basic structure for my backtrack search

--- src/BacktrackSearch.java
+++ src/BacktrackSearch.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+
 //This class will do the backtracking with FC and MRV search
 public class BacktrackSearch {
 
+        public static shiftDomains backtrackMRV(final SchedulingProblem  problem, final shiftDomains domains, final int nextHourToAssign){
+            boolean solution = true;
+
+            for(ArrayList<Integer> domain: domains.shiftDomains){
+                if(domain.size() != 1){
+                    solution = false;
+                    break;
+                }
+            }
+            if(solution) {
+                return domains;
+            }
+
+            //TODO: Implement search
+
+
+
+
+
+
+
+            return null;
+        }
+
+        public static Boolean isConsistent(){
+            //TODO: Run tests to see if a particular value assignment is consistent
+            return false;
+        }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
-
+//TODO: implement performance eval and verbosity parameter
 public class Solver {
 //same problem will be solved multiple times by both algorithms, so generate availabilities,
 // numEmployees, numShifts, and startTime here
     public static boolean runInstance(final int numShifts, final int numEmployees, final int verbosity){
         SchedulingProblem prob = new SchedulingProblem(numShifts, 0, numEmployees);
-        ArrayList<ArrayList<Boolean>> employees = prob.getEmployees();
+        ArrayList<ArrayList<Boolean>> employees = prob.getEmployees(); //you need to access the list of employees + availabilities from prob
         shiftDomains startDomains = new shiftDomains(0, 10,employees);
-        System.out.println(startDomains.toString());
-        return false;
+        System.out.println(startDomains.toString()); //for testing
+        shiftDomains result1 = BacktrackSearch.backtrackMRV(prob,startDomains,0);
+        //Andrew, call your search function here on the same startDomain
+        if(result1 != null){ //result2 (Andrew's search) will need to be checked too
+            System.out.println(result1);
+            return true;
+        }
+        else{
+            System.out.println("No solution found for result 1");
+            return false;
+        }
     }
 
 
